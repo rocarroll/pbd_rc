dcf0ea6253e45af0a9688f1bb393cabbaa008547
Roisin Carroll <roisincarroll@gmail.com>
2017-06-29 11:12:10 +0100

    spellcheck files

---
+++ spellchecker/spellcheck.py
@@ -0,0 +1,33 @@
+## spellchecker
+
+words = open("spell.words").readlines()
+
+words = map(lambda x: x.strip(), words)
+print len(words)
+print words[0]
+print words[len(words)-1]
+
+print ('zygotic' in words)
+print ('misdasdas' in words)
+
+def load_words(file_name):
+	words = open(file_name).readlines()
+	words = map(lambda x: x.strip(), words)
+	return words
+
+def check_word(words,word):
+	return word in words
+	
+def check_words(words,senteance):
+	words_to_check = senteance.split(' ')
+	for word in words_to_check:
+		if not check_word(words,word):
+			print ("Word is misspelt : " + word)
+			return False
+	return True
+	
+print load_words("spell.words") [0]
+
+print check_word(words, 'zygotic')
+print check_word(words, 'mistasdas')
+print (check_words(words,'zygotic mistasdas elementary'))
