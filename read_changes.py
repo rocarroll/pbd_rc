3396a8227bcfd556e9a8e2d3e2327534fbe9322c
Roisin Carroll <roisincarroll@gmail.com>
2017-07-14 16:33:40 +0100

    dictionary made

--- read_changes.py
+++ read_changes.py
@@ -16,16 +16,19 @@ while index < len(data):
 	try:
 		details = data[index+1].split(' | ')
 		#print details
-		commits.append(details)
+		commit = {'revision':details[0],'author':details[1],
+			'date':details[2]}
+		commits.append(commit)
 		index = data.index(sep, index+1)
 		
 	except IndexError:
 		break
 print len(commits)
 
+commits.revers()
 output_file = 'changes.csv'
 my_file = open(output_file, 'w')
 my_file.write('Revision,Author,Date \n') 
-for details in commits:
-	my_file.write(details[0] + ',' + details[1] + ',"' + details[2]+ '"\n')
+for commit in commits:
+	my_file.write(commit['revision']+','+ commit ['author']+',"'+commit['date'] + '"\n')
 my_file.close()
