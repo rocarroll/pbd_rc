2b5063fcc31c978e11052c2202c4c601bb9e9371
Roisin Carroll <roisincarroll@gmail.com>
2017-08-07 18:35:06 +0100

    added calculator done in R

---
+++ list calculator/calculatorR.R
@@ -0,0 +1,56 @@
+#1 Function for the addition calculation
+add <- function(first, second)
+  first + second
+
+add(5,3)
+
+#2 Function for the subtraction calculation.	
+subtract <- function(first, second)
+  first - second
+
+subtract(5,3)
+
+#3 Function for the division calculation.
+divide <- function(first, second)
+  first / second
+
+divide(5,3)
+
+#4 Function for the square calculation.	
+square <- function(first)
+  first * first
+
+square(3)
+
+#5 Function for the multiply calculation.
+multiply <- function(first, second)
+  first * second
+
+multiply(5,3)
+
+#6 Function for the square root calculation.
+squareroot<-function(first)
+  first**0.5
+
+squareroot(5)
+
+#7 Function for the exponent x calculation.
+exponentx<-function(first)
+  2.718**first
+
+exponentx(3)
+
+#8 Function for the degreeRadian calculation.
+degreesRadian <- function(first)
+  first*0.017453
+
+degreesRadian(3)
+
+#9Function for the modulo calculation.
+modulo <- function(first,second)
+  first%%second
+
+modulo (5,3)
+
+#10 Function for the factorial calculation.
+factorial(3)
